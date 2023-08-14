4306485002a6b373f71037abd0b915be37c98a3d
Ramiro Santos <ramiro.santosf40@google.com>
2023-08-13 23:02:45 -0500

    Can this work?

---
+++ Assets/Player Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    // Update is called once per frame
+    public float turnSmoothTime = 0.1f;
+    public float speed = 6.0f;
+    private Animator animator;
+    float turnSmoothVelocity;
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f){
+
+            //Calculates the angle at which my character is facing and the camera location. 
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            
+            //Adjustment for rotational movement
+            Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward ;
+            
+            //Moving Animations
+            if(!Input.GetKey(KeyCode.LeftShift)){
+                speed = 6.0f;
+                animator.SetBool("IsMoving", true);
+                animator.SetBool("IsRunning", false);
+            }else{
+                speed = 10f;
+                animator.SetBool("IsRunning", true);
+                animator.SetBool("IsMoving", false);
+            }
+            //Needs to be affected by Gravity Affecting the Y axis
+            moveDir.y = -100f * Time.deltaTime;
+
+            //Add a speed boost 
+            
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            Debug.Log(moveDir);
+        }else{
+            //Animation for the idle
+            animator.SetBool("IsMoving", false);
+            animator.SetBool("IsRunning", false);
+        }
+        
+    }
+}
