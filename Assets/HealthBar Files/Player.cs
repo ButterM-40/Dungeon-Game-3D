c4b9dd23b5052135bcc567ba53a0296e32d5bf67
Brandidon <brandon.tiu01@utrgv.edu>
2023-09-10 22:11:29 -0500

    Set up scripts for inventory and items, working on implementing ui and item pickup 9/10/23

--- Assets/HealthBar Files/Player.cs
+++ Assets/HealthBar Files/Player.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public InventoryObject inventory;
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Testing Collision on Item: " + other.name);
+        
+        var item = other.GetComponent<Item>();
+        if (other.CompareTag("Player"))
+        {
+            if (item)
+            {
+                inventory.AddItem(item.item, 1);
+                Destroy(other.gameObject);
+            }
+        }        
+    }
+    //public void OnCollisionEnter(Collision other)
+    //{
+    //    var item = other.GetComponent<Item>();
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        if (item)
+    //        {
+    //            inventory.AddItem(item.item, 1);
+    //            Destroy(other.gameObject);
+    //        }
+    //    }
+    //}
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
+    }
+
     public int maxHealth=100;
     public int currentHealth;
 
