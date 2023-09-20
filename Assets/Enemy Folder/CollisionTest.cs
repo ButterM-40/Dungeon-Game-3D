c57e92fcf959f97a503d1945befe82dd93a3bda6
DiegoAdame13322 <diegoadame1322@gmail.com>
2023-09-20 11:59:48 -0500

    Smarter Enemy Movement

---
+++ Assets/Enemy Folder/CollisionTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    
+    float AttackCD = 100000.0f;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            print("Enter");
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        float timePassed = Time.deltaTime;
+        if (other.gameObject.tag == "Player")
+        {
+            //if (timePassed >= AttackCD)
+            //{
+            other.GetComponent<Player>().TakeDamage(10);
+            print("Attack");
+            print(Time.deltaTime);
+            //timePassed=0;
+            //}
+        }
+    }
+}
---
+++ Assets/Enemy Folder/EnemyDamage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+public class EnemyDamage : MonoBehaviour
+{
+
+    public float enemyDamageAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
