b8ccc370d52b004d47e7d40a4b57e11588537a07
Ramiro Santos <Ramiro.santosf40@gmail.com>
2023-11-13 21:14:59 -0600

    More Stuff

--- Assets/Death/TeleportToBoss.cs
+++ Assets/Death/TeleportToBoss.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,3 +12,18 @@ public class TeleportToBoss : MonoBehaviour
         }
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportToBoss : MonoBehaviour
+{
+    void OnTriggerEnter(Collider player){
+        if(player.CompareTag("Player")){
+            SceneManager.LoadScene(1);
+        }
+    }
+}
+>>>>>>> 63f50c2d7a97a4281c8cff50ce74765b827a7a91
--- Assets/Player Scripts/buttons.cs
+++ Assets/Player Scripts/buttons.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,3 +13,19 @@ public class buttons : MonoBehaviour
         SceneManager.LoadScene(1);
    }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+public class buttons : MonoBehaviour
+{
+    public void ReturnToGame(){
+        SceneManager.LoadScene(0);
+   }
+   public void HomenGame(){
+        SceneManager.LoadScene(1);
+   }
+}
+>>>>>>> 63f50c2d7a97a4281c8cff50ce74765b827a7a91
